4eb5eef04bd585a4930d7d53d65b6fd115627269
Mahdi <mahdi.ahmadi7@outlook.com>
2024-04-13 23:33:25 +0200

    signup

---
+++ AspCore.MVC/ViewModels/SignUpViewModel.cs
@@ -0,0 +1,11 @@
+﻿using AspCore.MVC.Models;
+
+namespace AspCore.MVC.ViewModels;
+
+public class SignUpViewModel
+{
+
+    public string Title { get; set; } = "Sign Up";
+    public SignUpModel Form { get; set; } = new SignUpModel();
+   
+}
